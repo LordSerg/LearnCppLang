3250f3781419bae7ab66fa0fd8866ee24fcf45ca
LordSerg <oknelaksoms@gmail.com>
2024-03-13 03:33:38 +0200

    13.03.2024

--- LeetCode/10. RegExMatching/main.cpp
+++ LeetCode/10. RegExMatching/main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include <cstring>
 #include <string>
 #include <vector>
-#include <cmath>
+//#include <vector>
+//#include <cmath>
 /*
 Given an input string s and a pattern p, implement regular expression matching with support for '.' and '*' where:
 
@@ -24,47 +26,218 @@ Input: s = "ab", p = ".*"
 Output: true
 Explanation: ".*" means "zero or more (*) of any character (.)".
 */
+//p = pattern, s = string
+bool no_isMatch(std::string s, std::string p)
+{
+    bool isDot{p.find('.')!=std::string::npos};
+    bool isStar{p.find('*')!=std::string::npos};
+    //4 variants:
+    //1) p doesn't have neither '.' nor '*' in it
+    if(!isStar&&!isDot)
+    {
+        //check, if p equals to s
+        return (strcmp(p.c_str(),s.c_str())==0);
+    }
+    //2) p have '.' in it, but don't have '*'
+    else if(isDot&&!isStar)
+    {
+        if(p.size()==s.size())
+        {
+            for(int i{0};i<s.size();++i)
+            {
+                if(p[i]!='.')
+                {
+                    if(s[i]!=p[i])
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    //3) p have '*' in it, but don't have '.'
+    else if(!isDot&&isStar)
+    {
+        //every "**" = "*"
+        //and remember every "*" position
+        std::vector<int> stars{};
+        for(int i{};i<p.size()-1;++i)
+        {
+            if(p[i]=='*'&&p[i+1]=='*')
+            {
+                //check if there are more of them
+                int j{i+1};
+                while(j<p.size()&&p[j]=='*')
+                    ++j;
+                //delete from i to j-1 from p;
+                p.erase(i,j-i-1);
+                stars.push_back(i);
+                //i=0;
+            }
+            else if(p[i]=='*')
+            {
+                stars.push_back(i);
+            }
+        }
+        
+        /*
+        s = aaaaaaaaaaaaaaaqwerty
+        p = aaaa*aaaaaqwerty
+
+        p -> {aaaa, aaaaaqwerty}
+        foreach p:
+            if(s.contains(p))
+        */
+
+        for(int i{},j{};i<s.size()&&j<p.size();++i,++j)
+        {//i - is for s
+        //j - is for p
+            if(p[j]!='*')
+            {
+                if(s[i]!=p[j])
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                
+            }
+        }
+    }
+    //4) p has both '.' and '*' in it
+    else //if(isDot&&isStar)
+    {
+        //every "**" = "*"
+        for(int i{};i<p.size()-1;++i)
+        {
+            if(p[i]=='*'&&p[i+1]=='*')
+            {
+                //check if there are more of them
+                int j{i+1};
+                while(j<p.size()&&p[j]=='*')
+                    ++j;
+                //delete from i to j from p;
+                p.erase(i,j-i-1);
+                i=0;
+            }
+        }
+
+    }
+return false;
+}
 
 bool isMatch(std::string s, std::string p)
 {
-    //4 variants:
-    //1) p doesn't have neither '.' nor '*' in it
-    //2) p have '.' in it, but don't have '*'
-    //3) p have '*' in it, but don't have '.'
-    //4) p has both '.' and '*' in it
 
-return false;
-}
-bool isPrime(long long n)
-{
-    int f{};
-    for(long long i{1};i<=n/2;++i)
+    bool isDot{p.find('.')!=std::string::npos};
+    bool isStar{p.find('*')!=std::string::npos};
+    //1) p doesn't have neither '.' nor '*' in it
+    if(!isStar&&!isDot)
     {
-        if(n%i==0)
+        //check, if p equals to s
+        return (strcmp(p.c_str(),s.c_str())==0);
+    }
+    //2) p have '.' in it, but don't have '*'
+    else if(isDot&&!isStar)
+    {
+        if(p.size()==s.size())
         {
-            f+=2;
+            for(int i{0};i<s.size();++i)
+            {
+                if(p[i]!='.')
+                {
+                    if(s[i]!=p[i])
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+        else
+        {
+            return false;
         }
     }
-    return (f==2);
+    else
+    {
+        bool **dp = new bool*[s.size()+1];
+        for(int i{};i<s.size()+1;++i)
+            dp[i] = new bool[p.size()+1];
+
+        for(int i{};i<s.size()+1;++i)
+            for(int j{};j<p.size()+1;++j)
+                dp[i][j]=false;
+        
+        dp[s.size()][p.size()] = true;
+
+        for(int i{static_cast<int>(s.size())};i>=0;--i)
+        {
+            for(int j{static_cast<int>(p.size())-1};j>=0;--j)
+            {
+                bool first = (i<s.size()&&
+                            (p.at(j)==s.at(i) || p.at(j)=='.'));
+                if((j+1) < p.size() && p.at(j+1) == '*')
+                {
+                    dp[i][j] = dp[i][j+2] || first && dp[i+1][j];
+                }
+                else
+                {
+                    dp[i][j] = first && dp[i+1][j+1];
+                }
+            }
+        }
+
+        return dp[0][0];
+    }
+}
+
+void test(std::string s,std::string p)
+{
+    std::cout<<"string "<<s<< (isMatch(s,p)?" matches ":" doesn't match ")<< "pattern "<<p<<std::endl;
 }
 int main()
 {
-    std::vector<long long> a{1,4};
-    long long k{3};
-    long long factors{};
-    long long n{1};
-    while (n<50)
+    
+    test((std::string)("aa"),(std::string)("a"));
+    test((std::string)("aa"),(std::string)("a*"));
+    test((std::string)("aa"),(std::string)("a."));
+    test((std::string)("aa"),(std::string)(".*"));
+    test((std::string)("aaaaaaaaaaaaaaaqwerty"),(std::string)("aaaa*aaaaaqwerty"));
+    
+    /*
+    std::string p{"0****1234*567********89"};
+    std::vector<int> stars{};
+    for(int i{};i<p.size()-1;++i)
     {
-        if(isPrime(n))
+        if(p[i]=='*'&&p[i+1]=='*')
         {
-            long long aboba=1;
-            for(int i{};i<n-1;++i)
-            {
-                aboba*=2;
-            }
-            std::cout<<n<<" -> "<<aboba*3<<std::endl;
+            //check if there are more of them
+            int j{i+1};
+            while(j<p.size()&&p[j]=='*')
+                ++j;
+            //delete from i to j-1 from p;
+            p.erase(i,j-i-1);
+            stars.push_back(i);
+            //i=0;
         }
-        ++n;
+        else if(p[i]=='*')
+        {
+            stars.push_back(i);
+        }
     }
+    std::cout<<p<<std::endl;
+    for(int i{};i<stars.size();++i)
+    {
+        std::cout<<stars[i]<<" ";
+    }
+    */
+    //isMatch("qwer","qwer");
     return 0;
 }

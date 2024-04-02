9e7cf66ba49a229937bba849242b747132f73327
LordSerg <oknelaksoms@gmail.com>
2024-04-03 01:58:32 +0300

    03.04.2024

--- Olymp/a/ProblemB/main.cpp
+++ Olymp/a/ProblemB/main.cpp
@@ -62,6 +62,30 @@ void shiftTree(TreeNode*head)
         }
     }
 }
+long long int findMaxVect(std::vector<long long int>a)
+{ 
+    long long int ans = a[0];
+    for(auto i:a)
+    {
+        if(i>ans)
+            ans=i;
+    }
+    return ans;
+}
+
+void shift(std::vector<long long int>&a,std::vector<int>p,long long int &max)
+{
+    std::vector<long long int>tmp(a.size());
+    tmp[0] = a[0];
+    for(int i{0};i<p.size();++i)
+    {
+        tmp[p[i]]+=a[i+1];
+        if(tmp[p[i]]>max)
+            max = tmp[p[i]];
+    }
+    a=tmp;
+}
+
 int main()
 {
     // TreeNode*Jesse=new TreeNode(10);
@@ -71,7 +95,7 @@ int main()
     // dummy->addDistributors(std::vector<int>{19,64});
     // dummy = dummy->next[0];
     // dummy->addDistributors(std::vector<int>{36});
-    
+    /*
     TreeNode*Jesse=new TreeNode(25);
     TreeNode*dummy = Jesse;
     dummy->addDistributors(std::vector<int>{29});
@@ -93,6 +117,47 @@ int main()
         shiftTree(Jesse);
         std::cout<<"shift #"<<i<<std::endl;
         Jesse->show();
+        std::cout<<std::endl;
+    }
+    */
+    int t{};
+
+    std::cin>>t;
+    for(;t>0;--t)
+    {
+        int n{};
+        std::cin>>n;
+        std::vector<long long int> a;
+        std::vector<int> p;
+        if(n==1)
+        {
+            int fix;
+            std::cin>>fix;
+        }
+        for(int i{};i<n-1;i++)
+        {
+            int k;
+            std::cin>>k;
+            p.push_back(k);
+        }
+        for(int i{};i<n;i++)
+        {
+            int k;
+            std::cin>>k;
+            a.push_back(k);
+        }
+        long long int max{findMaxVect(a)};
+        //std::cout<<max<<" ";
+        for(int times{};times<n;++times)
+        {
+            //std::cout<<"shift #"<<times<<std::endl;
+            std::cout<<max<<" ";
+            shift(a,p,max);
+            //for(auto i:a)
+            //    std::cout<<i<<" ";
+            //std::cout<<std::endl;
+            //std::cout<<"max = "<<max<<std::endl;
+        }
         std::cout<<std::endl;
     }
     return 0;

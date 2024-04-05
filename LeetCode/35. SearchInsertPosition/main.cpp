2a315545d0b76e22641a212346153b7f36dbaee8
LordSerg <oknelaksoms@gmail.com>
2024-04-05 23:59:31 +0300

    04.05.2024

---
+++ LeetCode/35. SearchInsertPosition/main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+/*Given a sorted array of distinct integers and a target value, return the index if the target is found. 
+If not, return the index where it would be if it were inserted in order.
+
+You must write an algorithm with O(log n) runtime complexity.
+*/
+int searchInsert(std::vector<int>& nums, int target)
+{
+    int i=static_cast<int>(nums.size());
+    int l,r;
+    l=0;
+    r=i-1;
+    i/=2;
+    while(l!=r)
+    {
+        if(r-l==1)
+        {
+            if(nums[r]==target)
+                return r;
+            else if(nums[l]==target)
+                return l;
+            else if(nums[r]>target&&nums[l]>target)
+                return l-1;
+            else if(nums[r]<target&&nums[l]<target)
+                return r+1;
+            else //if(nums[r]<target&&nums[l]>target)
+                return r;
+            break;
+        }
+        if(nums[i]==target)
+            return i;
+        if(nums[i]>target)
+        {
+            r=i;
+            i=(l+r)/2;
+        }
+        else //if(nums[i]<target)
+        {
+            l=i;
+            i=(l+r)/2;
+        }
+        
+    }
+    return i;
+}
+int main()
+{
+    
+    return 0;
+}
